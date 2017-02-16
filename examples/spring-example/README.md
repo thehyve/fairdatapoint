0aa4be430fe79bf06c3e04ec9b952ac48d92c327
kburger <burger.github@gmail.com>
2017-02-16 16:31:12 +0100

    added metadata html view to example application

---
+++ examples/spring-example/README.md
@@ -0,0 +1,2 @@
+# Spring example
+Depends on the [fairdatapoint-html](https://github.com/kburger/fairdatapoint-html) project. 
--- examples/spring-example/src/main/java/com/github/kburger/fairdatapoint/example/spring/WebConfig.java
+++ examples/spring-example/src/main/java/com/github/kburger/fairdatapoint/example/spring/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.github.kburger.fairdatapoint.EnableFairDatapoint;
+import com.github.kburger.fairdatapoint.html.EnableFairDatapointHtml;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan
 @EnableWebMvc
 @EnableFairDatapoint
+@EnableFairDatapointHtml
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
