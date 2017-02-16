f0a8f64d683ea619f53d175443e4bc1f080bf546
kburger <burger.github@gmail.com>
2017-02-16 15:34:13 +0100

    updated package-info.java files

---
+++ src/main/java/com/github/kburger/fairdatapoint/io/annotation/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Annotation based metadata writer.
+ */
+package com.github.kburger.fairdatapoint.io.annotation;
---
+++ src/main/java/com/github/kburger/fairdatapoint/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Service interfaces that make up the main API.
+ */
+package com.github.kburger.fairdatapoint.service;
